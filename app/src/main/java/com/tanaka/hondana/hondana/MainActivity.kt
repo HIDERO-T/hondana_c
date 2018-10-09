fbd352c7eb81cf6b8fa1aca9e39968d20ff207f3
TANAKA <h-tanaka@r-learning.co.jp>
2018-10-09 17:04:40 +0900

    Barcode detecting finish.

--- app/src/main/java/com/tanaka/hondana/hondana/MainActivity.kt
+++ app/src/main/java/com/tanaka/hondana/hondana/MainActivity.kt
@@ -1,33 +1,10 @@
 package com.tanaka.hondana.hondana
 
-import android.Manifest
-import android.content.Intent
-import android.content.IntentFilter
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.hardware.Camera
-import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
-import android.support.v4.app.ActivityCompat
-import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
-import com.google.android.gms.vision.barcode.Barcode
-import com.google.android.gms.vision.barcode.BarcodeDetector
-import kotlinx.android.synthetic.main.activity_main.*
-import android.util.SparseArray
-import com.google.android.gms.vision.Frame
 import android.widget.TextView
 import android.widget.Toast
-import com.google.android.gms.vision.CameraSource
-import com.google.android.gms.vision.MultiProcessor
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,64 +15,37 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //FIXME: なぜか動かない。
-        //findViewById<ConstraintLayout>(R.id.operationBox).visibility = View.GONE
         setContentView(R.layout.activity_main)
-        /*
-        val rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-        if (rc == PackageManager.PERMISSION_GRANTED) {
-            createCameraSource(true, false)
-        } else {
-            requestCameraPermission()
+
+        val barcode: String = intent.getStringExtra("barcode")
+        applyData(barcode)
+
+        findViewById<Button>(R.id.buttonBorrow).setOnClickListener{
+            Toast.makeText(this, "借りました！", Toast.LENGTH_SHORT).show()
+            finish()
         }
-        */
 
-
-
-        findViewById<View>(R.id.scanner).setOnClickListener(object: View.OnClickListener {
-            override fun onClick(v: View){
-                //TODO: スキャナ完成したらイベントを変更。
-                val operationBox: ConstraintLayout = findViewById(R.id.operationBox)
-                if (operationBox.visibility == View.GONE)
-                    operationBox.visibility = View.VISIBLE
-                else
-                    operationBox.visibility = View.GONE
-
-            }
-        })
-
-        findViewById<FloatingActionButton>(R.id.toSettings).setOnClickListener{
-                val img: ImageView = findViewById<ImageView>(R.id.bimage)
-                val bmp: Bitmap = BitmapFactory.decodeResource(
-                                                applicationContext.resources, R.drawable.puppy)
-                img.setImageBitmap(bmp)
-
-                val detector = BarcodeDetector.Builder(applicationContext)
-                        .setBarcodeFormats(Barcode.DATA_MATRIX or Barcode.QR_CODE)
-                        .build()
-                if (!detector.isOperational) {
-                    Log.d("BARCODE", "Could not set up the detector!")
-                    //return
-                }
-                val frame = Frame.Builder().setBitmap(bmp).build()
-                val barcodes = detector.detect(frame)
-                val thisCode = barcodes.valueAt(0)
-                val txtView = findViewById(R.id.isbn) as TextView
-                txtView.text = thisCode.rawValue
-
-                //FIXME: 動かない
-                //val intent: Intent = Intent(this, barcodereader.BarcodeCaptureActivity::class.java)
-                //startActivity(intent)
-
-                //TODO: 上は本当はBarcodeDetected。下記に差し替える。
-                //val intent: Intent = Intent(this, SettingsActivity::class.java)
-                //startActivity(intent)
+        findViewById<Button>(R.id.buttonReturn).setOnClickListener{
+            Toast.makeText(this, "返しました！", Toast.LENGTH_SHORT).show()
+            finish()
         }
+
+
     }
 
-    fun applyData(isbn: Int){
-        //TODO: API接続処理
 
+
+    private fun applyData(isbn: String){
+        val bookstock = BookStock(isbn)
+        val bookinfo = BookInfo(isbn)
+        findViewById<TextView>(R.id.textIsbn).text = isbn
+        findViewById<TextView>(R.id.textTitle).text = bookinfo.title!!
+        findViewById<TextView>(R.id.textAuthor).text = bookinfo.author!!
+        findViewById<TextView>(R.id.numberAll).text = bookstock.numberAll!!.toString() + getString(R.string.book_unit)
+        findViewById<TextView>(R.id.numberOnloan).text = bookstock.numberOnloan!!.toString() + getString(R.string.book_unit)
+        findViewById<TextView>(R.id.numberAvailable).text = bookstock.numberAvailable!!.toString() + getString(R.string.book_unit)
+        findViewById<Button>(R.id.buttonBorrow).isEnabled = bookstock.canBorrow
+        findViewById<Button>(R.id.buttonReturn).isEnabled = bookstock.canReturn!!
     }
 
 
