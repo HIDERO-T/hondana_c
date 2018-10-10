0d86ce11a71955db0fef1bb033e6d2b992570c46
TANAKA <h-tanaka@r-learning.co.jp>
2018-10-10 18:32:38 +0900

    Temperature Design finished.

--- app/src/main/java/com/tanaka/hondana/hondana/BookUtil.kt
+++ app/src/main/java/com/tanaka/hondana/hondana/BookUtil.kt
@@ -1,5 +1,12 @@
 package com.tanaka.hondana.hondana
 
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import android.os.AsyncTask
+import java.io.*
+
+
 //TODO: Nullableでない設計に変更スべき。
 
 class Book(id: Int){
@@ -49,4 +56,69 @@ class BookInfo(isbn: String){
         title = "Ruby公式"
         author = "増井雄一郎"
     }
+}
+
+class HttpResponsAsync : AsyncTask<Void, Void, String>() {
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        // doInBackground前処理
+    }
+
+    override fun doInBackground(vararg params: Void): String? {
+        var con: HttpURLConnection? = null
+        var url: URL? = null
+        val urlSt = "http://125.12.14.155:3000/books/"
+
+        try {
+            // URLの作成
+            url = URL(urlSt)
+            // 接続用HttpURLConnectionオブジェクト作成
+            con = url!!.openConnection() as HttpURLConnection
+            // リクエストメソッドの設定
+            con!!.setRequestMethod("GET")
+            // リダイレクトを自動で許可しない設定
+            con!!.setInstanceFollowRedirects(false)
+            // URL接続からデータを読み取る場合はtrue
+            con!!.setDoInput(true)
+            // URL接続にデータを書き込む場合はtrue
+            con!!.setDoOutput(true)
+
+            // 接続
+            con!!.connect() // ①
+
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+
+        return null
+    }
+
+    override fun onPostExecute(result: String) {
+        super.onPostExecute(result)
+        // doInBackground後処理
+    }
+
+}
+
+@Throws(IOException::class, UnsupportedEncodingException::class)
+fun readInputStream(`in`: InputStream): String {
+    val sb = StringBuffer()
+    var st = ""
+
+    val br = BufferedReader(InputStreamReader(`in`, "UTF-8"))
+    while (st != null) {
+        sb.append(st)
+        st = br.readLine()
+    }
+    try {
+        `in`.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    return sb.toString()
 }
